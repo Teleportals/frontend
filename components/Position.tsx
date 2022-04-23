d4d392d3442d2944d7ccafd1ddae9546daff2cdd
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-04-23 15:31:08 +0200

    add: position style

--- components/Position.tsx
+++ components/Position.tsx
@@ -1,12 +1,23 @@
+import { Box, Flex } from "reflexbox"
 import styles from "./Position.module.css"
 
 export default function Position() {
   return (
     <div className={styles.container}>
-      <div>Ethereum</div>
-      <div>Aave</div>
-      <div>Collateral: 1 ETH</div>
-      <div>Borrowed: 0.6 ETH</div>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box>
+          <div className={styles.chainName}>Ethereum</div>
+          <div>Aave</div>
+        </Box>
+        <Box>
+          <div className={styles.label}>Collateral</div>
+          <div className={styles.value}>1 ETH</div>
+        </Box>
+        <Box>
+          <div className={styles.label}>Borrowed</div>
+          <div className={styles.value}>0.6 ETH</div>
+        </Box>
+      </Flex>
     </div>
   )
 }
