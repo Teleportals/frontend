42f271dace8fbe1861f3a6e818e6df973b01d462
Boyan Barakov <boyan.barakov@gmail.com>
2022-04-23 19:45:51 +0200

    try hooks in teleport

--- pages/teleport.tsx
+++ pages/teleport.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+import { useAccount, useContract } from "wagmi"
+//import { ERC20_ABI } from "../globals/abis"
+import { tokensRinkeby, tokensKovan } from "../globals/tokens"
+import { providerRinkeby, providerKovan } from "../globals/providers"
 import Position from "../components/Position"
 import styles from "./teleport.module.css"
 
 function Teleport() {
+  const [{ data }] = useAccount({})
+  const [positions, setPositions] = useState([]);
+
+  //const providerAave = useContract({
+    //addressOrName: '',
+    //contractInterface: PROVIDER_AAVE_ABI,
+    //signerOrProvider: providerRinkeby,
+  //});
+
+  //const providerCompound = useContract({
+    //addressOrName: '',
+    //contractInterface: PROVIDER_COMPOUND_ABI,
+    //signerOrProvider: providerKovan,
+  //});
+
+  //const contract = useContract({
+    //addressOrName: tokensRinkeby.WETH.address,
+    //contractInterface: ERC20_ABI,
+    //signerOrProvider: providerRinkeby,
+  //});
+
+  useEffect(() => {
+    async function fetch() {
+      // call FloanProviderAave getPairBalances(collAddr, debtAddr, userAddr)
+      // call FloanProviderCompound
+      // setPositions()
+    }
+    if (data && data.address) fetch();
+  }, [data]);
+
   return (
     <div>
       <h1 className={styles.title}>Select a position to teleport</h1>
